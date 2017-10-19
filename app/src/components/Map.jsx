599e0b30221e3e92ac78ec5a938a6122ddadcc3e
Fliko <baxter.flw@gmail.com>
2017-10-19 00:15:23 -0500

    can once again add to list from map

--- app/src/components/Map.jsx
+++ app/src/components/Map.jsx
@@ -8,12 +8,29 @@ class Map extends React.Component {
     this.map;
   }
   componentDidMount() {
-    this.map = new this.props.Maps({lat:29.4241, lng: 98.4936}, 13, document.getElementById('map'),document.getElementById('search'));
+    let mapObj = {center: {lat:29.4241, lng: -98.4936}, zoom: 11};
+    let map = document.getElementById('map');
+    let search = document.getElementById('search');
+    this.map = new this.props.Maps(mapObj, map, search);
+  }
+  handleClick(e) {
+    e.preventDefault();
+    let name = document.getElementById('mapName').value;
+    let data = {center: this.map.getCenter(),
+                zoom: this.map.getZoom(),
+              };
+    this.props.saveMap(name, data);
   }
   render(){
-    return(<div id='map' style={{height:"500px"}}>
-      <input id='search' type="text"></input>
-    </div>);
+    return(
+      <div>
+        <input id="mapName" type='text' placeholder='name your map'></input>
+        <div id='map' style={{height:"500px"}}>
+          <input id='search' type="text"></input>
+        </div>
+        <button onClick={this.handleClick.bind(this)}>Save Map</button>
+      </div>
+    );
   }
 }
 
--- app/src/container/MapInterface.jsx
+++ app/src/container/MapInterface.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { saveMap } from '../actions/actions';
+import { addRoute } from '../actions/actions';
 import Map from '../components/Map.jsx';
 
-let MapApi = (center, zoom, mapElement, searchElement) => {
-  let map = new google.maps.Map(mapElement, {
-    center: center,
-    zoom: zoom
+let MapApi = function(mapObj, mapElement, searchElement) {
+  this.map = new google.maps.Map(mapElement, {
+    center: mapObj.center,
+    zoom: mapObj.zoom
   });
-  map.controls[google.maps.ControlPosition.TOP_RIGHT].push(searchElement);
-  let autocomplete = new google.maps.places.Autocomplete(searchElement);
+  this.map.controls[google.maps.ControlPosition.TOP_RIGHT].push(searchElement);
+  this.autocomplete = new google.maps.places.Autocomplete(searchElement);
 }
+
+MapApi.prototype.getCenter = function() {
+  return this.map.getCenter();
+};
+
+MapApi.prototype.getZoom = function() {
+  return this.map.getZoom();
+};
 
 const mapStateToProps = state => ({
     mapState: state.curMap,
